d0978050891471f2eb7f5004c67e751e6a1323d8
William Ye <w3ye@pm.me>
2021-07-04 22:16:49 -0400

    sayHelloToConsole and returnSayHello added

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
-const sayHello = function(name) {
+const sayHelloToConsole = function(name) {
   console.log(`Hello, ${name}`);
 }
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
