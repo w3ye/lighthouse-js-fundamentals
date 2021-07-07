a7de7608b2a6d09353451eb45704ee8bcbeb18bf
William Ye <w3ye@pm.me>
2021-07-07 15:09:29 -0400

    added chorus-for.js and conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,18 +1,20 @@
-// const raining = true;
-// const temperature = 12;
+const raining = true;
+const temperature = 12;
 
-// if (raining) {
-//   console.log("Don't forget your umbrella");
-// }
-// if (temperature < 0) {
-//   console.log("Make sure you pick out a scaf!");
-// } else if (temperature < 15){
-//   console.log("Short sleeves won't cut it");
-// } else {
-//   console.log("Short sleeves are fine");
-// }
+if (raining) {
+  console.log("Don't forget your umbrella");
+}else if (!raining) {
+  console.log("Leave your unbrella at home")
+}
+if (temperature < 0) { 
+  console.log("Make sure you pick out a scaf!");
+} else if (temperature < 15){
+  console.log("Short sleeves won't cut it");
+} else {
+  console.log("Short sleeves are fine");
+}
 
-// console.log("Now you're ready to go outside");
+console.log("Now you're ready to go outside");
 
 const isCitizen = true;
 const age = 26;
