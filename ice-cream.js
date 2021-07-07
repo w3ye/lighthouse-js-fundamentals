bb692ebab4610036d33e064027d9256ffe2f16eb
William Ye <w3ye@pm.me>
2021-07-07 15:48:53 -0400

    added ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+console.log(iceCreamFlavours.length);
